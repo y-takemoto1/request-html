09f958ae0eb82d636e4805392db623e41623bb02
takemoto <y-takemoto@kts-it.com>
2024-10-24 17:51:34 +0900

    test

--- sele.py
+++ sele.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -5,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import streamlit as st
+
 
 st.title("seleniumテストアプリ")
 
