2f30341715861b68b30bbbeb3ef0bf647f4d86b2
takemoto <y-takemoto@kts-it.com>
2024-10-30 13:29:12 +0900

    test

--- sele.py
+++ sele.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import streamlit as st
 
 # Serviceオブジェクトを作成
 service = Service(ChromeDriverManager().install())
@@ -10,9 +11,11 @@ driver = None  # driverを初期化（Noneで設定）
 # WebDriverを初期化
 try:
     driver = webdriver.Chrome(service=service)
-    driver.get("https://www.example.com")
+    driver.get("https://www.google.co.jp/")
+    st.text('ブラウザが正常に起動しました。')
     print("ブラウザが正常に起動しました。")
 except Exception as e:
+    st.text('エラーが発生しました：', e)
     print("エラーが発生しました:", e)
 finally:
     if driver is not None:  # driverが初期化されている場合のみquit
